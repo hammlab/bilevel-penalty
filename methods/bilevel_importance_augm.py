2af21640b8dfeb02bfc92172ac04925fc726aaad
jihunhamm <jihun.hamm@gmail.com>
2018-12-10 13:13:17 -0500

    Add files via upload

--- methods/bilevel_importance_augm.py
+++ methods/bilevel_importance_augm.py
@@ -1,3 +1,4 @@
+## Allow dropout and data augmentation
 import sys
 import tensorflow as tf
 import numpy as np
@@ -6,15 +7,21 @@ from bilevel_penalty import *
 
 class bilevel_importance(object):
 
-    def __init__(self,sess,x_train_tf,x_test_tf,y_train_tf,y_test_tf,cls_train,cls_test,var_cls,batch_size,lr_u,lr_v,rho_0,lamb_0,eps_0,c_rho,c_lamb,c_eps,istraining_tf):
+    def __init__(self,sess,x_train_tf,x_train_augm_tf,x_test_tf,x_test_augm_tf,y_train_tf,y_test_tf,
+        cls_train,cls_train_augm,cls_test,cls_test_augm,var_cls,
+        batch_size,lr_u,lr_v,rho_0,lamb_0,eps_0,c_rho,c_lamb,c_eps,istraining_tf):
     
         self.sess = sess
         self.x_train_tf = x_train_tf
+        self.x_train_augm_tf = x_train_augm_tf
         self.x_test_tf = x_test_tf
+        self.x_test_augm_tf = x_test_augm_tf
         self.y_train_tf = y_train_tf
         self.y_test_tf = y_test_tf
         self.cls_train = cls_train
+        self.cls_train_augm = cls_train_augm
         self.cls_test = cls_test
+        self.cls_test_augm = cls_test_augm
         self.var_cls = var_cls # v
         self.batch_size = batch_size
         self.istraining_tf = istraining_tf
@@ -24,13 +31,13 @@ class bilevel_importance(object):
         self.assign_importance_atanh = tf.assign(self.importance_atanh,self.importance_atanh_tf)
         importance = 0.5*(tf.tanh(self.importance_atanh) + 1.) # between 0 and 1
 
-        self.f = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.cls_test,labels=self.y_test_tf))
-        self.g = tf.reduce_sum(tf.multiply(importance,tf.nn.softmax_cross_entropy_with_logits(logits=self.cls_train,labels=self.y_train_tf)))/tf.reduce_sum(importance)
+        self.f = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.cls_test_augm,labels=self.y_test_tf))
+        self.g = tf.reduce_sum(tf.multiply(importance,tf.nn.softmax_cross_entropy_with_logits(logits=self.cls_train_augm,labels=self.y_train_tf)))/tf.reduce_sum(importance)
         
         self.bl = bilevel_penalty(sess,self.f,self.g,self.importance_atanh,var_cls,lr_u,lr_v,rho_0,lamb_0,eps_0,c_rho,c_lamb,c_eps)
         
-        self.loss_simple = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.cls_train,labels=self.y_train_tf))
-        self.optim_simple = tf.train.AdamOptimizer(lr_v).minimize(self.loss_simple,var_list=self.var_cls)
+        self.loss_simple_augm = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.cls_train_augm,labels=self.y_train_tf))
+        self.optim_simple_augm = tf.train.AdamOptimizer(lr_v).minimize(self.loss_simple_augm,var_list=self.var_cls)
         
         #self.sess.run(self.init)
         #self.sess.run(tf.global_variables_initializer())
@@ -40,15 +47,19 @@ class bilevel_importance(object):
     def train(self, x_train, y_train, x_test, y_test, importance_atanh, niter=1):
         self.sess.run(self.assign_importance_atanh,feed_dict={self.importance_atanh_tf:importance_atanh})
 
-        feed_dict={self.x_train_tf:x_train, self.y_train_tf:y_train, self.x_test_tf:x_test, self.y_test_tf:y_test}
+        feed_dict={self.x_train_augm_tf:x_train, self.y_train_tf:y_train, self.x_test_augm_tf:x_test, self.y_test_tf:y_test}
         f,gvnorm,lamb_g = self.bl.train(feed_dict,niter)
         timp_atanh = self.sess.run(self.importance_atanh)
 
         return [f,gvnorm,lamb_g,timp_atanh]
 
 
-
-    def train_simple(self, x_train, y_train, nepochs):
+    def train_simple(self, x_train, y_train, nepochs, x_test=None, y_test=None):
+        if x_test is None:
+            x_test = x_train
+        if y_test is None:
+            y_test = y_train
+            
         batch_size = self.batch_size#x_train_tf.get_shape()[0]
         Ntrain = x_train.shape[0]
         nb_batches = int(np.floor(float(Ntrain)/batch_size))
@@ -58,12 +69,16 @@ class bilevel_importance(object):
             for batch in range(nb_batches):
                 ind_batch = range(batch_size*batch,min(batch_size*(1+batch),Ntrain))
                 ind_tr = ind_shuf[ind_batch]                
-                self.sess.run(self.optim_simple,feed_dict={self.x_train_tf:x_train[ind_tr,:],self.y_train_tf:y_train[ind_tr,:]})
+                self.sess.run(self.optim_simple_augm,feed_dict={self.x_train_augm_tf:x_train[ind_tr,:],self.y_train_tf:y_train[ind_tr,:]})
             if epoch%10==0:
-                l = self.sess.run(self.loss_simple,feed_dict={self.x_train_tf:x_train[ind_tr,:],self.y_train_tf:y_train[ind_tr,:],self.istraining_tf:False})
+                l = self.sess.run(self.loss_simple_augm,feed_dict={self.x_train_augm_tf:x_train[ind_tr,:],self.y_train_tf:y_train[ind_tr,:],self.istraining_tf:False})
                 print('epoch=%d/%d, loss=%f'%(epoch,nepochs,l))
-
-        return self.eval_simple(x_train, y_train)
+                print('train:')
+                self.eval_simple(x_train, y_train)
+                print('test:')
+                self.eval_simple(x_test, y_test)
+        #return self.eval_simple(x_test, y_test)
+        return
 
 
 
@@ -77,7 +92,7 @@ class bilevel_importance(object):
             print('Warning: Data size is not a multiple of batch_size!!!!')
         for batch in range(nb_batches):
             ind_batch = range(batch_size*batch,min(batch_size*(1+batch),Ntest))
-            pred = self.sess.run(self.cls_test, {self.x_test_tf:x_test[ind_batch,:],self.istraining_tf:False})
+            pred = self.sess.run(self.cls_test,{self.x_test_tf:x_test[ind_batch,:],self.istraining_tf:False})
             acc += np.sum(np.argmax(pred,1)==np.argmax(y_test[ind_batch,:],1))
             preds[ind_batch] = np.argmax(pred,1)
         acc /= np.float32(nb_batches*batch_size)
